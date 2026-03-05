2037bcad895605550c7138ba5c766381391a1bde
WillMaac <anderrwil@gmail.com>
2026-03-05 14:13:07 -0300

    feat: criação do serviço de API com Axios

---
+++ ProductShowcase/src/api/pokemonService.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://pokeapi.co/api/v2",
+});
+
+//  O endpoint de lista retorna apenas { name, url }
+
+export const getPokemonList = async (limit: number = 151) => {
+  const response = await api.get(`/pokemon?limit=${limit}`);
+  return response.data;
+};
+
+export const getPokemonByName = async (name: string) => {
+  const response = await api.get(`/pokemon/${name}`);
+  return response.data;
+};
